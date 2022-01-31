dab55ac5a5c71655f901955e6487a7134906437b
Micha Davis <m.leigh.davis@gmail.com>
2022-01-31 12:42:54 -0800

    added question logic functions

--- js/app.js
+++ js/app.js
@@ -33,3 +33,77 @@ let oArr = [
   ['2 million','100','So Many','10,000'],
   ['','','',''],
 ];
+
+
+//#region DOM Handles
+
+let questionBox = document.getElementById('questions');
+let answerBox =  document.getElementById('input');
+let answerA = document.getElementById('answer-a');
+let answerB = document.getElementById('answer-b');
+let answerC = document.getElementById('answer-c');
+let answerD = document.getElementById('answer-d');
+
+//#endregion
+
+//#region Question Constructor & Prototype
+
+function Question(question, answer, optionsArr) {
+  this.question = question;
+  this.answer = answer;
+  this.optionsArr = optionsArr;
+
+  Question.list.push(this);
+}
+
+Question.list = [];
+
+//#endregion
+
+//#region Global Functions
+
+// Populating Questions List
+function constructQuestions(){
+  for (let i in qArr){
+    new Question(qArr[i], aArr[i], oArr[i]);
+  }
+}
+
+// Choosing a question
+function chooseQuestion(){
+  let index = randomIntInclusive(0, Question.list.length);
+  let question = Question.list.splice(index, 1);
+  serveQuestion(question[0]);
+}
+
+// Displaying A Question
+function serveQuestion(question){
+  questionBox.firstChild.textContent = question.question;
+  answerA.textContent = question.optionsArr[0];
+  answerB.textContent = question.optionsArr[1];
+  answerC.textContent = question.optionsArr[2];
+  answerD.textContent = question.optionsArr[3];
+}
+
+
+// 2. collect input
+function handleGuess(){
+
+}
+
+// 3. compare input to answer
+
+// 4. calculate score
+
+
+function randomIntInclusive(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+//#endregion
+answerBox.addEventListener('click', handleGuess);
+//#region Event Listener
+
+
+
+
