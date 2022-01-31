4f4dac0e7b41f65ad65d993a435b25d73eed4315
Micha Davis <m.leigh.davis@gmail.com>
2022-01-31 11:56:32 -0800

    added pseudocode to app.js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,45 @@
-'use strict'
+'use strict';
+
+let qArr = [];
+let aArr = [];
+let oArr = [[]];
+
+
+
+
+
+
+
+
+
+
+
+
+//<<<<<STOP>>>>>
+
+let question = {
+  question1: {
+    q: 'question',
+    a: 'a',
+    o: ['options']
+  }
+};
+// question constructor
+function Question() {
+
+}
+
+// for loop i
+// this q = qarray[i]
+// this a = aarray[i]
+// thos o = oarray[i]
+
+// answer array for each q.
+
+// 1. serve the question
+
+// 2. collect input
+
+// 3. compare input to answer
+
+// 4. calculate score
