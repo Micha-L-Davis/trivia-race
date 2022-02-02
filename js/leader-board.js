413fdd97ca3bf97e49e7d108bd078faceb71e8cc
Micha Davis <m.leigh.davis@gmail.com>
2022-02-01 20:08:34 -0800

    fixed save bug

--- js/leader-board.js
+++ js/leader-board.js
@@ -1,10 +1,19 @@
 'use string';
 
-// retreive data from local storage
-let retrievePlayer = localStorage.getItem('leaderboard');
+// retrieve data from local storage
+let leaderboard = [];
+updateLeaderboard();
 
-let parsedPlayer = JSON.parse('retrievePlayer');
+function updateLeaderboard() {
+  let retrieveLeaderboard = localStorage.getItem('leaderboard');
 
+  if(retrieveLeaderboard) {
+    leaderboard = JSON.parse(retrieveLeaderboard);
+  }
+
+  let stringifiedLeaderboard = JSON.stringify(leaderboard);
+  localStorage.setItem('leaderboard', stringifiedLeaderboard);
+}
 // THIS IS JUST A TEST ***********
 // let parsedPlayer = [];
 
@@ -35,26 +44,30 @@ let parsedPlayer = JSON.parse('retrievePlayer');
 // render the table on the scoreboard
 // render name, score, and date
 
+// After sorting, pop off any extras until the top three are left
+while (leaderboard.length > 3){
+  leaderboard.pop();
+}
+
 function scoreBoard() {
 
   const table = document.querySelector('table');
 
-  for (let i = 0; i <= 3; i++) {
-
+  for (let i = 0; i < leaderboard.length; i++) {
 
     let tr = document.createElement('tr');
     table.appendChild(tr);
 
     let td1 = document.createElement('td');
-    td1.textContent = parsedPlayer[i].date;
+    td1.textContent = leaderboard[i].date;
     tr.appendChild(td1);
 
     let td2 = document.createElement('td');
-    td2.textContent = parsedPlayer[i].name;
+    td2.textContent = leaderboard[i].name;
     tr.appendChild(td2);
 
     let td3 = document.createElement('td');
-    td3.textContent = parsedPlayer[i].score;
+    td3.textContent = leaderboard[i].score;
     tr.appendChild(td3);
 
   }
