f4789b3b8d83213f6ab1ed2bdcab77d9077f0925
Micha Davis <m.leigh.davis@gmail.com>
2022-02-03 11:20:43 -0800

    code cleanup and comments

--- js/app.js
+++ js/app.js
@@ -4,7 +4,7 @@
 
 let choice = document.getElementById('player-select');
 let qPara = document.getElementById('q-para');
-let answerForm =  document.getElementById('input');
+let answerForm = document.getElementById('input');
 let answerA = document.getElementById('answer-a');
 let answerB = document.getElementById('answer-b');
 let answerC = document.getElementById('answer-c');
@@ -48,44 +48,44 @@ let qArr = [
 
 let difficultyArr = [2, 2, 3, 3, 1, 1, 2, 1, 3, 2, 2, 3, 3, 3, 2, 2, 1, 3, 3, 2, 3, 1, 1, 3, 3, 3];
 
-let aArr = ['c','a','d','a','b','b','c','b','a','d','d','c','a','b','a','d','c','d','d','a','b','c','d','a','b','c'];
+let aArr = ['c', 'a', 'd', 'a', 'b', 'b', 'c', 'b', 'a', 'd', 'd', 'c', 'a', 'b', 'a', 'd', 'c', 'd', 'd', 'a', 'b', 'c', 'd', 'a', 'b', 'c'];
 
 let oArr = [
-  ['Tiger','Bear','Hippopotamus','Elephant'],
-  ['Paws','Ears','Eyes','Nose'],
-  ['1 foot','3 feet','10 inches','1 inch'],
-  ['Kid','Baby','Calf','Doe'],
-  ['Elephant','Blue Whale','Bear','Giraffe'],
-  ['Calf','Doe','Lady','Cow'],
-  ['Nose','Skin','Tongue','Eyes'],
-  ['King Cobra','Anaconda','Rattlesnake','Python'],
-  ['On their legs','On their head','On their tail','On their feet'],
-  ['2 feet','50 feet','6 inches','10 feet'],
-  ['80 years','1 day','10 hours','28 days'],
-  ['1,000','100','30,000','35 million'],
-  ['2 million','100','So Many','10,000'],
-  ['Ringo Starr','John Lennon','Paul McCartney','George Harrison'],
-  ['Cat','Mongoose','Dragon','Parrot'],
-  ['Cow','Pig','Geese','Goats'],
-  ['Snowball','Icicle','A Rock','Flower'],
-  ['The Golden Eagle','The Osprey','The Duck','The Peregrine Falcon'],
-  ['The elephant and the lion','The bat and the raccoon','The hedgehog and the gopher','The anteater and the duck-billed platypus.'],
-  ['Bee Hummingbird','Finch','Yellow Canary','Lovebird'],
-  ['Clown Fish','Jellyfish','Sea Urchin','Giant Squid'],
-  ['Loch Ness Monster','Humpback Whale','Orca Whale','None'],
-  ['Their ancestral DNA was changed 50 years ago','Flamingos are extinct','Flamingos are not pink','Due to their main source of food being shrimp and algae'],
-  ['Ostrich','Owl','Bald Eagle','Vulture'],
-  ['Tail','Head','Abdomen','They do not have a heart'],
-  ['Hippopotamus','Rhinoceros','Elephant','Polar Bear'],
+  ['Tiger', 'Bear', 'Hippopotamus', 'Elephant'],
+  ['Paws', 'Ears', 'Eyes', 'Nose'],
+  ['1 foot', '3 feet', '10 inches', '1 inch'],
+  ['Kid', 'Baby', 'Calf', 'Doe'],
+  ['Elephant', 'Blue Whale', 'Bear', 'Giraffe'],
+  ['Calf', 'Doe', 'Lady', 'Cow'],
+  ['Nose', 'Skin', 'Tongue', 'Eyes'],
+  ['King Cobra', 'Anaconda', 'Rattlesnake', 'Python'],
+  ['On their legs', 'On their head', 'On their tail', 'On their feet'],
+  ['2 feet', '50 feet', '6 inches', '10 feet'],
+  ['80 years', '1 day', '10 hours', '28 days'],
+  ['1,000', '100', '30,000', '35 million'],
+  ['2 million', '100', 'So Many', '10,000'],
+  ['Ringo Starr', 'John Lennon', 'Paul McCartney', 'George Harrison'],
+  ['Cat', 'Mongoose', 'Dragon', 'Parrot'],
+  ['Cow', 'Pig', 'Geese', 'Goats'],
+  ['Snowball', 'Icicle', 'A Rock', 'Flower'],
+  ['The Golden Eagle', 'The Osprey', 'The Duck', 'The Peregrine Falcon'],
+  ['The elephant and the lion', 'The bat and the raccoon', 'The hedgehog and the gopher', 'The anteater and the duck-billed platypus.'],
+  ['Bee Hummingbird', 'Finch', 'Yellow Canary', 'Lovebird'],
+  ['Clown Fish', 'Jellyfish', 'Sea Urchin', 'Giant Squid'],
+  ['Loch Ness Monster', 'Humpback Whale', 'Orca Whale', 'None'],
+  ['Their ancestral DNA was changed 50 years ago', 'Flamingos are extinct', 'Flamingos are not pink', 'Due to their main source of food being shrimp and algae'],
+  ['Ostrich', 'Owl', 'Bald Eagle', 'Vulture'],
+  ['Tail', 'Head', 'Abdomen', 'They do not have a heart'],
+  ['Hippopotamus', 'Rhinoceros', 'Elephant', 'Polar Bear'],
 ];
 
-let playerCoords = [['45px', '250px', 'rear'],['80px', '80px', 'rear'],['275px','50px', 'right'],['480px','80px', 'right'],['510px','259px', 'front'],['460px','400px', 'front'],['275px','440px', 'left'],['80px','420px', 'left'], ['45px', '250px', 'rear']];
+let playerCoords = [['45px', '250px', 'rear'], ['80px', '80px', 'rear'], ['275px', '50px', 'right'], ['480px', '80px', 'right'], ['510px', '259px', 'front'], ['460px', '400px', 'front'], ['275px', '440px', 'left'], ['80px', '420px', 'left'], ['45px', '250px', 'rear']];
 
-let opponentCoords = [['125px', '250px', 'rear'],['140px', '130px', 'rear'],['250px','125px', 'right'],['420px','130px', 'right'],['450px','250px', 'front'],['420px','380px', 'front'],['300px','400px', 'left'],['150px','375px', 'left'], ['125px', '250px', 'rear']];
+let opponentCoords = [['125px', '250px', 'rear'], ['140px', '130px', 'rear'], ['250px', '125px', 'right'], ['420px', '130px', 'right'], ['450px', '250px', 'front'], ['420px', '380px', 'front'], ['300px', '400px', 'left'], ['150px', '375px', 'left'], ['125px', '250px', 'rear']];
 
 //#endregion
 
-//#region Question Constructor & Prototype
+//#region Object Constructors
 
 function Question(question, answer, optionsArr, difficulty) {
   this.question = question;
@@ -114,36 +114,44 @@ function Racer(image, name, animal) {
 
 //#region Runtime Code
 
-
+// global variables
+let easyQsCorrect = 0;
+let medQsCorrect = 0;
+let hardQsCorrect = 0;
+let totalQuestions = 0;
 let currentQuestion;
+let player;
+let opponent = new Racer('img/racer-img/chicken_rear.png', 'Cluck-U', 'chicken');
+
 constructQuestions();
 chooseQuestion();
-let player;
-let opponent = new Racer('img/racer-img/chicken_rear.png','Cluck-U', 'chicken');
 
 //#endregion
 
 //#region Global Functions
 
 // Populating Questions List
-function constructQuestions(){
-  for (let i=0; i < qArr.length; i++){
+function constructQuestions() {
+  for (let i = 0; i < qArr.length; i++) {
     new Question(qArr[i], aArr[i], oArr[i], difficultyArr[i]);
   }
 }
 
-// Choosing a question
-function chooseQuestion(){
-  let index = randomIndex();
+// Sets current question to question spliced from question list. Refills the question list if empty.
+function chooseQuestion() {
+  let index = randomIndex(questionList);
 
-  if (questionList){
+  if (questionList) {
     currentQuestion = questionList.splice(index, 1)[0];
     serveQuestion(currentQuestion);
   }
+  else {
+    constructQuestions();
+  }
 }
 
-// Displaying A Question
-function serveQuestion(question){
+// Displays a Question
+function serveQuestion(question) {
   qPara.textContent = question.question;
   answerA.textContent = question.optionsArr[0];
   answerB.textContent = question.optionsArr[1];
@@ -151,116 +159,143 @@ function serveQuestion(question){
   answerD.textContent = question.optionsArr[3];
 }
 
-
-// question difficulty counter and total questions asked variables
-let easyQsCorrect = 0;
-let medQsCorrect = 0;
-let hardQsCorrect = 0;
-let totalQuestions = 0;
-
-// Answer Event Handler
-function handleGuess(event){
+// Event handler for feedback message confirmation
+function handleConfirm(event) {
   event.preventDefault();
-  if(event.target.type === 'button' && event.target.name === 'answer'){
-    totalQuestions++;
-    console.log(totalQuestions);
-    if (event.target.value === currentQuestion.answer){
-      console.log('correct!');
-      document.getElementById('correct').style.display='flex';
-      player.score++;
-      if (currentQuestion.difficulty === 1){
-        easyQsCorrect++;
-      }
-      if (currentQuestion.difficulty === 2){
-        medQsCorrect++;
-      }
-      if (currentQuestion.difficulty === 3){
-        hardQsCorrect++;
-      }
-      console.log(currentQuestion.difficulty);
-      console.log(player.score);
-    }
-    else{
-      console.log('incorrect!');
-      document.getElementById('incorrect').style.display='flex';
+  if (event.target.type === 'button' && event.target.name !== 'answer') {
+    switch (event.target.name) {
+    case 'correct-answer':
+      document.getElementById('correct').style.display = 'none';
+      break;
+    case 'incorrect-answer':
+      document.getElementById('incorrect').style.display = 'none';
+      break;
+    case 'you-win':
+      document.getElementById('winner').style.display = 'none';
+      break;
+    case 'you-lose':
+      document.getElementById('loser').style.display = 'none';
+      break;
+    default:
+      break;
     }
 
-    let aiAnswer = randomIntInclusive(0, 1);
-
-    if (aiAnswer === 0){
-      console.log('AI guesses correctly');
-      opponent.score++;
+    if (player.score >= 8 || opponent.score >= 8) {
+      endRace();
     }
     else {
-      console.log('AI guesses incorrectly');
+      chooseQuestion();
     }
-    console.log(opponent.score);
+  }
+}
+
+// Answer event handler
+function handleGuess(event) {
+  event.preventDefault();
+  if (event.target.type === 'button' && event.target.name === 'answer') {
+    let targetValue = event.target.value;
+    judgePlayerGuess(targetValue);
+
+    judgeAIGuess();
 
     advanceRacers(player, opponent);
   }
 }
 
-// Player choice event handler
-function handleSubmit(event){
+// Player starting choices event handler
+function handleSubmit(event) {
   event.preventDefault();
 
-  console.log(event);
   let playerName = event.target[0].value;
   let playerImage;
   let playerAnimal;
   for (let i = 1; i <= 5; i++) {
-    if (event.target[i].checked === true){
+    if (event.target[i].checked === true) {
       playerAnimal = event.target[i].value;
       playerImage = `img/racer-img/${playerAnimal}_rear.png`;
     }
   }
   player = new Racer(playerImage, playerName, playerAnimal);
   advanceRacers(player, opponent);
-  document.getElementById('pregame').style.display='none';
-  document.getElementById('live-play').style.display='flex';
+  document.getElementById('pregame').style.display = 'none';
+  document.getElementById('live-play').style.display = 'flex';
 }
 
-function advanceRacers(player, opponent){
+// Find out if player's guess is good, add to weighted list, inform player of result
+function judgePlayerGuess(targetValue) {
+  totalQuestions++;
+  if (targetValue === currentQuestion.answer) {
+    document.getElementById('correct').style.display = 'flex';
+    player.score++;
+    if (currentQuestion.difficulty === 1) {
+      easyQsCorrect++;
+    }
+    if (currentQuestion.difficulty === 2) {
+      medQsCorrect++;
+    }
+    if (currentQuestion.difficulty === 3) {
+      hardQsCorrect++;
+    }
+  }
+  else {
+    document.getElementById('incorrect').style.display = 'flex';
+  }
+}
+
+// Flips a coin to see if the AI guesses correctly
+function judgeAIGuess() {
+  let aiAnswer = randomIntInclusive(0, 1);
+
+  if (aiAnswer === 0) {
+    console.log('AI guesses correctly');
+    opponent.score++;
+  }
+}
+
+// Sets racer object's coordinate properties to the position that matches their current score
+function advanceRacers(player, opponent) {
   player.positionX = playerCoords[player.score][0];
   player.positionY = playerCoords[player.score][1];
   player.image = `img/racer-img/${player.animal}_${playerCoords[player.score][2]}.png`;
   opponent.positionX = opponentCoords[opponent.score][0];
   opponent.positionY = opponentCoords[opponent.score][1];
   opponent.image = `img/racer-img/${opponent.animal}_${opponentCoords[opponent.score][2]}.png`;
-  renderRacers(player,opponent);
+  renderRacers(player, opponent);
 }
 
-function renderRacers(player, opponent){
+// Renders racer images at the object's coordinate property value
+function renderRacers(player, opponent) {
   playerImg.src = player.image;
   playerImg.style.left = player.positionX;
-  playerImg.style.top =player.positionY;
+  playerImg.style.top = player.positionY;
   opponentImg.src = opponent.image;
   opponentImg.style.left = opponent.positionX;
   opponentImg.style.top = opponent.positionY;
 
 }
 
-//renderRacers(playerImg, opponentImg);
-
-function endRace(){
+// Notify player of win or loss and tallies final score
+function endRace() {
   livePlay.removeEventListener('click', handleConfirm);
-  if (opponent.score > player.score){
-    document.getElementById('loser').style.display='flex';
+  if (opponent.score > player.score) {
+    document.getElementById('loser').style.display = 'flex';
   }
   else {
-    document.getElementById('winner').style.display='flex';
+    document.getElementById('winner').style.display = 'flex';
   }
-  player.score = (((1*easyQsCorrect) + (2*medQsCorrect) + (3*hardQsCorrect)) / totalQuestions) * 1000;
+  player.score = (((1 * easyQsCorrect) + (2 * medQsCorrect) + (3 * hardQsCorrect)) / totalQuestions) * 1000;
   storeData();
   updateLeaderboard();
 
 }
 
+// Make sure we have a LB array
 let leaderboard = [];
+// Retrieves any existing leaderboard data, appends recent playthrough data, and saves to local storage
 function updateLeaderboard() {
   let retrieveLeaderboard = localStorage.getItem('leaderboard');
 
-  if(retrieveLeaderboard) {
+  if (retrieveLeaderboard) {
     leaderboard = JSON.parse(retrieveLeaderboard);
   }
 
@@ -272,56 +307,29 @@ function updateLeaderboard() {
   localStorage.setItem('leaderboard', stringifiedLeaderboard);
 }
 
+// Returns a random int inclusive of the max bounds
 function randomIntInclusive(min, max) {
   return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
-function randomIndex() {
-  return Math.floor(Math.random() * questionList.length);
+// Returns a random index from the supplied array
+function randomIndex(arr) {
+  return Math.floor(Math.random() * arr.length);
 }
 
+// Save player object into local storage
 function storeData() {
   console.log('saving');
   let playerData = JSON.stringify(player);
   localStorage.setItem('playerData', playerData);
 }
 
-function handleConfirm(event){
-  event.preventDefault();
-  if(event.target.type === 'button' && event.target.name !== 'answer'){
-
-    switch (event.target.name) {
-    case 'correct-answer':
-      document.getElementById('correct').style.display='none';
-      break;
-    case 'incorrect-answer':
-      document.getElementById('incorrect').style.display='none';
-      break;
-    case 'you-win':
-      document.getElementById('winner').style.display='none';
-      break;
-    case 'you-lose':
-      document.getElementById('loser').style.display='none';
-      break;
-    default:
-      break;
-    }
-
-    if (player.score >= 8 || opponent.score >= 8){
-      endRace();
-    }
-    else{
-      console.log('why are we even here?');
-      chooseQuestion();
-    }
-  }
-}
 //#endregion
 
-//#region Event Listener
+//#region Event Listeners
 
+livePlay.addEventListener('click', handleConfirm);
 answerForm.addEventListener('click', handleGuess);
 choice.addEventListener('submit', handleSubmit);
-livePlay.addEventListener('click', handleConfirm);
 
 //#endregion
